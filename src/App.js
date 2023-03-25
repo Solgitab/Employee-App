c66534cf9cfacd0ba4729d3449acd25e89d2d07a
Selamneh K <selamnehmk@gmail.com>
2023-03-24 20:51:18 -0400

    src/App.js is modified with SMK initial

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          #1 <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> SMK
         </p>
         <a
           className="App-link"
